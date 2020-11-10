f06bf3dac92d925abe7decd55819adbdf72bf044
shun0305 <shunbbra@icloud.com>
2020-11-10 22:31:55 +0900

    losic

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
+$(document).ready(function () {
+    animateDiv($('.gu'));
+    animateDiv($('.cho'));
+    animateDiv($('.pa'));
+
+});
+
+function makeNewPosition() {
+    const h = $(window).height() + 600 - 800;
+    const w = $(window).width() - 100;
+
+    const nh = Math.floor(Math.random() * h);
+    const nw = Math.floor(Math.random() * w);
+
+    return [nh, nw];
+
+}
+
+function animateDiv(obj) {
+    const anime = makeNewPosition();
+    const anime_m = $(obj).offset();
+    const speed = calcSpeed([anime_m.top, anime_m.left], anime);
+    $(obj).animate({ top: anime[0], left: anime[1] }, speed, function () {
+        animateDiv(obj);
+    });
+}
+
+
+
+function calcSpeed(prev, next) {
+
+    const x = Math.abs(prev[0] - next[0]);
+    const y = Math.abs(prev[1] - next[1]);
+
+    const greatest = x > y ? x : y;
+
+    const speedModifier = 0.1;
+
+    const speed = Math.floor(greatest / speedModifier);
+
+    return speed;
+
+}
+
+
 const sorting = [
     {
         question: '皆さんがどの寮に入るか組み分けをします。<br>グリフィンドール、ハッフルパフ、レイブンクロー、そしてスリザリン。',
@@ -48,12 +93,17 @@ document.getElementById('gub').textContent = sorting[0].answers[0]
 document.getElementById('chb').textContent = sorting[0].answers[1]
 document.getElementById('pab').textContent = sorting[0].answers[2]
 
-const closeimg = $('.harryhat');
-const closingtext = $('.titlebox');
-const runa = $('.runa');
-const runatext = $('.runatext');
-const guri = $('.guri');
-const guritext = $('.guritext');
+const closeimg = $('.harryhat');//真ん中帽子
+const closingtext = $('.titlebox');//タイトル
+const runa = $('.runa');//レイブンクロー
+const runatext = $('.runatext');//ラッキー
+const guri = $('.guri');//グリフィンドール
+const guritext = $('.guritext');//おめでとう
+const gameover = $('.sryz'); //スリザリン
+const gameovertext = $('.finish');//ゲームオーバー
+const gubutton = document.getElementById('#gu_btn')
+const chbutton = document.getElementById('#cho')
+const pabutton = document.getElementById('.pa')
 
 
 //グーを押した場合
@@ -96,7 +146,9 @@ $(function () {
                     closeimg.fadeOut(0);
                     guritext.fadeIn(5000);
                     closingtext.fadeOut(0);
-
+                    gubutton.fadeOut(0);
+                    chbutton.fadeOut(0);
+                    pabutton.fadeOut(0);
 
                 }
 
@@ -116,46 +168,50 @@ $(function () {
 $(function () {
     $('#chb').on('click', function () {
         const randomnumber = Math.floor(Math.random() * 10);
-        if (randomnumber > 2) {
+        if (randomnumber < 10) {
             $("#cmp").html(sorting[0].response[0]);
             document.getElementById('gub').textContent = sorting[1].answers[0];
             document.getElementById('chb').textContent = sorting[1].answers[1];
             document.getElementById('pab').textContent = sorting[1].answers[2];
             count = 1
-        } else if (randomnumber < 1) {
+        } else if (randomnumber > 7) {
             $("#cmp").html(sorting[0].response[1])
         }
+        $('#chb').on('click', function () {
+            const randomnumber = Math.floor(Math.random() * 10);
+            if (randomnumber < 10) {
+                $("#cmp").html(sorting[1].response[0]);
+                document.getElementById('gub').textContent = sorting[2].answers[0];
+                document.getElementById('chb').textContent = sorting[2].answers[1];
+                document.getElementById('pab').textContent = sorting[2].answers[2];
 
+            }
+            $('#chb').on('click', function () {
+                const randomnumber = Math.floor(Math.random() * 10);
+                if (randomnumber < 10) {
+                    $("#cmp").html(sorting[2].response[1]);
+                    gameover.fadeIn(4000);
+                    closeimg.fadeOut(0);
+                    gameovertext.fadeIn(4000);
+                    closingtext.fadeOut(0);
+
+                }
+
+            });
+
+        });
     });
-});
-$(function () {
-    $('#chb').on('click', function () {
-        const randomnumber = Math.floor(Math.random() * 10);
-        if (randomnumber > 2) {
-            $("#cmp").html(sorting[1].response[0]);
-            document.getElementById('gub').textContent = sorting[2].answers[0];
-            document.getElementById('chb').textContent = sorting[2].answers[1];
-            document.getElementById('pab').textContent = sorting[2].answers[2];
 
-        } else if (randomnumber < 1) {
-            $("#cmp").html(sorting[1].response[0])
-        }
-
-    });
 });
 
 //パーを押した場合
 
-const gameover = $('.sryz'); //画像
-const gameovertext = $('.finish');
-const gubutton = document.getElementById('.gu')
-const chbutton = document.getElementById('.cho')
-const pabutton = document.getElementById('.pa')
+
 
 $(function () {
     $('#pab').on('click', function () {
         $("#cmp").html(sorting[1].response[1]);
-        gameover.fadeIn(4000); //画像がフワッと（fadeIn）現れて
+        gameover.fadeIn(4000);
         closeimg.fadeOut(0);
         gameovertext.fadeIn(4000);
         closingtext.fadeOut(0);
@@ -201,49 +257,7 @@ $(function () {
 
 
 
-$(document).ready(function () {
-    animateDiv($('.gu'));
-    animateDiv($('.cho'));
-    animateDiv($('.pa'));
 
-});
-
-function makeNewPosition() {
-    const h = $(window).height() + 600 - 800;
-    const w = $(window).width() - 100;
-
-    const nh = Math.floor(Math.random() * h);
-    const nw = Math.floor(Math.random() * w);
-
-    return [nh, nw];
-
-}
-
-function animateDiv(obj) {
-    const anime = makeNewPosition();
-    const anime_m = $(obj).offset();
-    const speed = calcSpeed([anime_m.top, anime_m.left], anime);
-    $(obj).animate({ top: anime[0], left: anime[1] }, speed, function () {
-        animateDiv(obj);
-    });
-}
-
-
-
-function calcSpeed(prev, next) {
-
-    const x = Math.abs(prev[0] - next[0]);
-    const y = Math.abs(prev[1] - next[1]);
-
-    const greatest = x > y ? x : y;
-
-    const speedModifier = 0.1;
-
-    const speed = Math.floor(greatest / speedModifier);
-
-    return speed;
-
-}
 
 
 
